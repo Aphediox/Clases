35e926dac0bcd5121a5fe67b05c62d4b1fc9dadf
Salvador Torrres <jorge.torres4847@alumnos.udg.mx>
2020-10-15 14:47:50 -0500

    Prueba Recuperar

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include "laboratorio.cpp"
 main(){
-    
+    laboratorio Lab2;
+    Lab2.recuperar();
+    Lab2.mostrar();
+    /*
     computadora cp1;
     std::string aux;
     aux= "Windowos 10";
@@ -15,23 +18,23 @@ main(){
     
     computadora pc3;
     std::cin >> pc3;
-    //std::cout << std::endl;
-    //std::cout << pc3 << std::endl;
-    //std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << pc3 << std::endl;
+    std::cout << std::endl;
     
     laboratorio l1;
     
-    //l1.agregarCP(cp2);
-    //l1.agregarCP(cp1);
+    l1.agregarCP(cp2);
+    l1.agregarCP(cp1);
 
     l1 << pc3 << cp2 << cp1;
     
-    //l1.mostrar();
-    l1.respaldar_tabla();
-    //std::cout << cp2;
-    l1.respaldar();
-    //l1.recuperar();
     l1.mostrar();
+    l1.respaldar_tabla();
+    std::cout << cp2;
+    l1.respaldar();
+    l1.mostrar();
+    */
     return 0;
 
 }
